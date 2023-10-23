9335423496e56c399d13ab59ff996e573ae13718
Sdn003 <ssudharsan210@gmail.com>
2023-10-24 02:06:57 +0530

    Added A new method

--- eclipse_to_git/src/main/java/eclipse_to_git_package/JustAClass.java
+++ eclipse_to_git/src/main/java/eclipse_to_git_package/JustAClass.java
@@ -1,10 +1,15 @@
 package eclipse_to_git_package;
 
 public class JustAClass {
+	
+	
+	public static void addingANewMethod() {
+		System.out.println("Hell New Method");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.print("Hello Git");
+		addingANewMethod();
 	}
 
 }
