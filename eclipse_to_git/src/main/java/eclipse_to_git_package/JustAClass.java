62550305a5840cc994856e9005ff25c97905c3ad
Sdn003 <ssudharsan210@gmail.com>
2023-10-24 02:04:40 +0530

    "Initial Commit"

---
+++ eclipse_to_git/src/main/java/eclipse_to_git_package/JustAClass.java
@@ -0,0 +1,10 @@
+package eclipse_to_git_package;
+
+public class JustAClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Git");
+	}
+
+}
